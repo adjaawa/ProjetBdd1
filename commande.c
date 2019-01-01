4334f570cbc49cdadfa1fd053ad8d6672cf13881
adjaawa <45859929+adjaawa@users.noreply.github.com>
2019-01-01 22:09:55 +0000

    Update commande.c

--- commande.c
+++ commande.c
@@ -1,117 +1,154 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//Verifier les paramétres d'entrées(Json ou XML)
-int verifierInput(int argc,char const *argv[]){
-	int i;
-	for (i = 1; i < argc ; i++)
-	{
-		if(strcmp("-i",argv[i])==0){ //Verification de l'input i
-            if(i+1<argc){
-            	if(strcmp("xml",argv[i+1])==0 ){// Pour un fichier xml
-            		return 1;
-            	}else if (strcmp("json",argv[i+1])==0){// Pour un fichier json
-            		return 2;
-            	}
-            }
-		}
-	}
-	return 0;
-}
-//Verifier la trace 
-int Trace(int argc,char const *argv[]){
-	int i;
-	for (i = 1; i < argc ; i++)
-	{
-		if(strcmp("-t",argv[i])==0){
-            return 1;
-		}
-	}
-	return 0;
-}
-//
-int verifierOutput(int argc,char const *argv[]){
 
-	int i;
-	int t;
-	for (i = 1; i < argc ; i++)
+//Verifier l'extension de l'entrée (Json ou XML)
+int verifierInput(int argc,char const *argv[])
 	{
-		if(strcmp("-o",argv[i])==0){
-            if(i+1<argc){
-            	t = strlen(argv[i+1]);
-            	if(t>4){
+		int i;
+		for (i = 1; i < argc ; i++)
+		{
+			if(strcmp("-i",argv[i])==0)
+				{  
+					//Verification de l'input i
+					if(i+1<argc)
+						{
+							if(strcmp("xml",argv[i+1])==0)
+								{
+									// Pour un fichier xml
+									return 1;
+								}
+							else if (strcmp("json",argv[i+1])==0)
+								{
+									// Pour un fichier json
+									return 2;
+								}
+						}
+				}
+		}
+		return 0;
+	}
+
+//Verifie si l'utilisateur veut les traces 
+int Trace(int argc,char const *argv[])
+	{
+		int i;
+		for (i = 1; i < argc ; i++)
+			{
+				if(strcmp("-t",argv[i])==0)
+					{
+						return 1;
+					}
+			}
+		return 0;
+	}
+
+//Vérifie que le fichier de sortie a l'extension '.svg'
+int verifierOutput(int argc,char const *argv[])
+	{
+
+		int i;
+		int t;
+		for (i = 1; i < argc ; i++)
+			{
+				if(strcmp("-o",argv[i])==0)
+					{
+            					if(i+1<argc)
+							{
+								t = strlen(argv[i+1]);
+								if(t>4)
+									{
             		
-            		if(argv[i+1][t-1]=='g' && argv[i+1][t-2]=='v' && argv[i+1][t-3]=='s' && argv[i+1][t-4]=='.'){
-            			return 1;
-            		}
-            	}
-            }
-		}
+										if(argv[i+1][t-1]=='g' && argv[i+1][t-2]=='v' && argv[i+1][t-3]=='s' && argv[i+1][t-4]=='.')
+											{
+												return 1;
+            										}
+            								}
+          						}
+					}
+			}
+			return 0;
 	}
-	return 0;
-}
-char* getName(int argc,char const *argv[]){
-	int i;
-	for (i = 1; i < argc ; i++)
-	{
-		if(strcmp("-o",argv[i])==0){
-			return (char *)argv[i+1];
-		}
-	}
-	return NULL;
-}
-char* Input_Fichier(int argc,char const *argv[]){
-	int i;
-	for (i = 1; i < argc ; i++)
-	{
-		if(strcmp("-f",argv[i])==0){
 
-
-
-			return (char *)argv[i+1];
-		}
-	}
-	return 0;
-}
-char* Input_Flux(int argc,char const *argv[]){
-	int i;
-	for (i = 1; i < argc ; i++)
+//Recupère le nom du fichier de sortie
+char* getName(int argc,char const *argv[])
 	{
-		if(strcmp("-h",argv[i])==0){
-		  }	return (char *)argv[i+1];
-		}
+		int i;
+		for (i = 1; i < argc ; i++)
+			{
+				if(strcmp("-o",argv[i])==0)
+					{
+						return (char *)argv[i+1];
+					}
+			}
+		return NULL;
 	}
+
+//Recupère le nom du l'input s'il est de type fichier
+char* Input_Fichier_Flux(int argc,char const *argv[])
+	{
+		int i;
+		for (i = 1; i < argc ; i++)
+			{
+				if(strcmp("-f",argv[i])==0 || strcmp("-h",argv[i])==0 )
+					{
+						return (char *)argv[i+1];
+					}
+			}
+		return 0;
+	}
+
+//Recupere le nom de l'input si c'est un flux http
+/*char* Input_Flux(int argc,char const *argv[])
+	{
+		int i;
+		for (i = 1; i < argc ; i++)
+			{
+				if(strcmp("-h",argv[i])==0)
+					{
+		  				return (char *)argv[i+1];
+					}
+			}
 	return 0 ;
-}
+	}	*/
+	
+
 int main(int argc,char const *argv[])
 {
 	
 	int failed = 0;
-	if(!verifierInput(argc,argv)){
+	if (!verifierInput(argc,argv))
+		{
 		printf("Vous n'avez pas renseinger l'option -i [-i xml/json]\n");
 		failed=1;
 	}
-	if(!verifierOutput(argc,argv)){
+	
+	if (!verifierOutput(argc,argv))
+		{
 		printf("Vous n'avez pas renseiger l'option -o [-o nomfichier.svg]\n");
 		failed=1;
 	}
-	if(!verifierInput(argc,argv)){
-		printf("Vous n'avez pas renseinger l'option -f [-f FichierInput]\n");
+	if (!Input_Fichier_Flux(argc,argv))
+		{
+		printf("Vous n'avez pas renseinger l'option -f [-f FichierInput] ou -h [-h url_HTTP]\n");
 		failed=1;
 	}
-	if(!verifierInput(argc,argv)){
+	/*if (!Input_Flux(argc,argv))
+		{
 		printf("Vous n'avez pas renseinger l'option -h [-h url_HTTP]\n");
 		failed=1;
-		}	
+	}	*/
 
 
-	if(!failed){
+	if(!failed) {
 		FILE* file = NULL;
 		file = fopen(getName(argc,argv),"w+");
-		printf("Reussi\n");
-		if(Trace(argc,argv)){
-			printf("Affichage de la trace!");
+		if(Trace(argc,argv)==1)
+			{
+			printf("Success.\nLes traces seront affichées.\n ");
 		}
+		else
+			printf("Success\nLes traces ne seront pas affichées\n");
 	}
 	return 0;
 }
