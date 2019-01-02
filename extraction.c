f3d26eff26697ff5b45ab9ec4bb7da9171c36146
Ndoumbe SOW <34397191+NSSOW@users.noreply.github.com>
2019-01-02 22:58:17 +0000

    extraction des données

---
+++ extraction.c
@@ -0,0 +1,35 @@
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+ 
+void print_content( xmlNode* root )
+{
+  xmlNode* current = NULL;
+  for ( current=root; current!=NULL; current=current->next )
+  {
+    if ( current->type==XML_ELEMENT_NODE )
+      printf( "node type: %s\n", current->name );
+    if ( strcmp((char*)current->name,"labyrinth")==0 )
+    {
+      printf( "  length: %s\n", (const char*)xmlGetProp( current, (const xmlChar*)"length" ) );
+      printf( "  width:  %s\n", (const char*)xmlGetProp( current, (const xmlChar*)"width"  ) );
+      printf( "  name:   %s\n", (const char*)xmlGetProp( current, (const xmlChar*)"name"   ) );
+    }
+    print_content( current->children );
+  }
+}
+ 
+int main( int, char** )
+{
+  xmlDoc*  doc  = NULL;
+  xmlNode* root = NULL;
+  doc = xmlReadFile( "xml_test.xml", NULL, 0 );
+   
+  if ( doc==NULL )
+    return 0;
+   
+  root = xmlDocGetRootElement( doc );
+  print_content( root );
+  xmlFreeDoc( doc );
+  xmlCleanupParser();
+   
+  return 1;
