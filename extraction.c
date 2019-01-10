6ca60373984c2509df576a64317102844f833163
Ndoumbe SOW <34397191+NSSOW@users.noreply.github.com>
2019-01-10 01:37:24 +0000

    Update extraction.c

--- extraction.c
+++ extraction.c
@@ -1,37 +1,114 @@
 #include <stdio.h>
-#include <libxml/parser.h>
+#include <string.h>
 #include <stdlib.h>
-#include <libxml/tree.h>
- 
-void print_content( xmlNode* root )
+#include <libxml/parser.h>
+
+//declaration de la strcuture node et ses varables 
+struct node {
+        char *nomEntite;
+        char *id;
+        char *nom;
+        char *prenom;
+        char *association;
+       char  *date;
+
+        struct node *next;
+            };
+        struct node *head = NULL;
+//declaration de la fonction insert et Initialisation des variables a,b,c,d et e
+void insert(char *a, char *b, char *c, char *d, char *e, char *f){
+        struct node *temp;
+        temp = (struct node*)malloc(sizeof(struct node));
+        temp->nomEntite = a;
+        temp->id = b;
+        temp->nom = c;
+        temp->prenom = d;
+        temp->association = e;
+        temp->date = f;
+        temp->next = head;
+        head= temp;
+
+
+}    
+// declaration de la fonction traverse_dom_trees qui permet de parcourir les noeuds et ensuite afficher leur valeur
+void traverse_dom_trees(xmlNode * a_node)
 {
-  xmlNode* current = NULL;
-  for ( current=root; current!=NULL; current=current->next )
-  {
-    if ( current->type==XML_ELEMENT_NODE )
-      printf( "node type: %s\n", current->name );
-    if (strcmp((char*)current->name,"labyrinth")==0)
+    xmlNode *cur_node = NULL;
+
+    for (cur_node = a_node; cur_node; cur_node = cur_node->next)
     {
-      printf( "  length: %s\n", (const char*)xmlGetProp( current, (const xmlChar*)"length" ) );
-      printf( "  width:  %s\n", (const char*)xmlGetProp( current, (const xmlChar*)"width"  ) );
-      printf( "  name:   %s\n", (const char*)xmlGetProp( current, (const xmlChar*)"name"   ) );
-    }
-    print_content( current->children );
-  }
-}
+                char *pol, *dev, *stat, *srcz, *dstz;
+                if(strcmp((const char *)cur_node->name,(const char *)"nomEntite") == 0){
+                 xmlChar *a = xmlNodeGetContent(cur_node);
+                        printf("Nom entite: %s\n", a);
+                      
+                        }
+             else if(strcmp((const char *)cur_node->name,(const char *)"id") == 0)  {
+                    xmlChar *b = xmlNodeGetContent(cur_node);
+                        printf("ID : %s\n", b);
+                        dev = strdup(b);
+                       
+                        }
+            else if(strcmp((const char *)cur_node->name,(const char *)"nom") == 0){
+                    xmlChar *c = xmlNodeGetContent(cur_node);
+                        printf("NOM : %s\n", c);
+                        stat = strdup(c);
+                       
+                        }
+            else if(strcmp((const char *)cur_node->name,(const char *)"prenom") == 0){
+                    xmlChar *d = xmlNodeGetContent(cur_node);
+                        printf("PRENOM : %s\n", d);
+                        srcz = strdup(d);
+                        
+                        }
+           
+                else if(strcmp((const char *)cur_node->name,(const char *)"association") == 0){
+                        xmlChar *e = xmlNodeGetContent(cur_node);
+                         printf("ASSOCIATION : %s\n", e);
+                        dstz = strdup(e);
+                        }
+               else if(strcmp((const char *)cur_node->name,(const char *)"date") == 0){
+                        xmlChar *f = xmlNodeGetContent(cur_node);
+                         printf("DATE : %s\n", f);
+                        }
+
  
-int main( int a, char** b)
+
+        traverse_dom_trees(cur_node->children);
+    }
+}
+int main(int argc, char **argv)
 {
-  xmlDoc*  doc  = NULL;
-  xmlNode* root = NULL;
-  doc = xmlReadFile( "assoc.xsd", NULL, 0 );
-   
-  if ( doc==NULL )
+    xmlDocPtr doc;
+    xmlNode *roo_element = NULL;
+//VERIFICATION DU NOMBRE D'ARGUMENT
+    if (argc != 2)
+    {
+        printf("\nArgument Invalid \n");
+        return(1);
+    }
+
+//VERIFICATION DE LA VALIDITE DU DOCUMENT CONTENANT LE CODE XML PASSE EN PARAMETRE
+    doc = xmlReadFile(argv[1], NULL, XML_PARSE_NOBLANKS | XML_PARSE_NOERROR | XML_PARSE_NOWARNING | XML_PARSE_NONET);
+    if (doc == NULL)
+    {
+        fprintf(stderr, "Document invalide.\n");
+        return 0;
+    }
+
+    roo_element = xmlDocGetRootElement(doc);
+//ON VERIFIE SI LE DOCUMENT CONTENANT LE CODE XML PASSE EN PARAMETRE EST VIDE
+    if (roo_element == NULL)
+ {
+        fprintf(stderr, "Document vide\n");
+        xmlFreeDoc(doc);
+        return 0;
+    }
+
+    printf("Root Node is %s\n", roo_element->name);
+    traverse_dom_trees(roo_element);
+
+    xmlFreeDoc(doc);       // free document
+    xmlCleanupParser();    // Free globals
     return 0;
-   
-  root = xmlDocGetRootElement( doc );
-  print_content( root );
-  xmlFreeDoc( doc );
-  xmlCleanupParser();  
-return 1;
 }
