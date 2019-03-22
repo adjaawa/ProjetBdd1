50a1bdfda0d0a5aa67c35075ebbe916b36fbdcd1
adjaawa <45859929+adjaawa@users.noreply.github.com>
2019-03-22 16:26:32 +0000

    JSON_CHECKER header file

--- JSON_checker.h
+++ JSON_checker.h
@@ -7,20 +7,16 @@ typedef struct JSON_checker_struct {
 } * JSON_checker;
 
 extern JSON_checker new_JSON_checker(int depth);
-
 /*
     Make a new JSON_checker. You indicate the maximum depth that is allowed.
     It will return an object that you will pass to the other functions.
     They will destroy the object for you.
 */
-
 extern int JSON_checker_char(JSON_checker jc, int next_char);
-
 /*
     You should call JSON_checker_char for each character of the JSON text.
     It will return false if the text is not right.
 */
-
 extern int JSON_checker_done(JSON_checker jc);
 
 /*
